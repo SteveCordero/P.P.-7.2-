2ce8c4115d913f5a477bd83c499e1bdcad9bbfee
Steve Cordero <stevecords2008@gmail.com>
2021-02-12 04:01:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+    Sports ruggleball = new Sports("Ruggleball","Smoothies", 55 , true, false);
+
+    System.out.println(ruggleball.toString());
+  }//end main method
+}//end main class aka the driver
---
+++ Sports.java
@@ -0,0 +1,31 @@
+public class Sports
+{
+  public String sport;
+  public String team;
+  public int score;
+  public boolean isTeam;
+  public boolean isWinner;
+
+  //Constructors
+  public Sports(String sport, String team, int score, boolean isTeam, boolean isWinner)
+  {
+    this.team = team;
+    this.sport = team;
+    this.score = score;
+    this.isTeam = isTeam;
+    this.isWinner = isWinner;
+  }//end constructor
+
+  //toString
+  public String toString()
+  {
+    return "Team = " + team + 
+            "\nSport = " + sport +
+            "\nScore = " + score +
+            "\nIs it a Team Sport = " + isTeam +
+            "\nDid they win = " + isWinner;
+  }
+
+
+
+}
